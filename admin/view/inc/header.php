b15499bc4ac74cb3f3caf9deb5294d22503365a3
Cédric <cmouleyre@axome.com>
2014-01-20 13:10:27 +0100

    CDN bitch

--- admin/view/inc/header.php
+++ admin/view/inc/header.php
@@ -5,18 +5,19 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title>miniOrm Admin</title>
 	
-	<script src="lib/jquery.min.js"></script>
-	<script src="lib/jquery-ui/jquery-ui.min.js"></script>
-	<script src="lib/bootstrap/js/bootstrap.min.js"></script>
-	<script src="lib/ckeditor/ckeditor.js"></script>
-	<script src="lib/ckeditor/adapters/jquery.js"></script>
-	<script src="lib/ckeditor/select2/select2.js"></script>
+	<script src="//code.jquery.com/jquery-git2.js"></script>
+	<script src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.2/ckeditor.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.2/adapters/jquery.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/select2/3.4.5/select2.min.js"></script>
 	<script src="asset/js/miniOrm.js"></script>
 	
-	<link href="lib/bootstrap/css/bootstrap.min.css" media="screen" rel="stylesheet">
-	<link href="lib/select2/select2.css" media="screen" rel="stylesheet">
-	<link href="lib/select2/select2-bootstrap.css" media="screen" rel="stylesheet">
-	<link href="lib/jquery-ui/jquery-ui.min.css" media="screen" rel="stylesheet">
+	<link href="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.2/contents.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+	<link href="//cdnjs.cloudflare.com/ajax/libs/select2/3.4.5/select2.min.css" media="screen" rel="stylesheet">
+	<link href="//cdnjs.cloudflare.com/ajax/libs/select2/3.4.5/select2-bootstrap.css" media="screen" rel="stylesheet">
+	<link href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet">
 	
 </head>
 <body>
