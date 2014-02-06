852ed9dee8e7c1cb880e942ff9ff1f9c6bf465b3
Cédric Mouleyre <mouleyrecedric@gmail.com>
2014-02-06 08:31:55 +0100

    Simple Ckeditor

--- admin/asset/miniOrm.js
+++ admin/asset/miniOrm.js
@@ -1,8 +1,18 @@
 $(function() {
+	
 	$(".input-datetime").datepicker();
-	$("textarea.form-control").ckeditor();
+	
+	$("textarea.form-control").ckeditor({
+	        toolbar:
+	        [
+	            ['Bold', 'Italic', 'Underline', '-', 'NumberedList', 'BulletedList', '-', 'Link'],
+	            ['UIColor']
+	        ]
+	});
+	
 	$(".select").select2({
 		placeholder: "Select a table",
 	    width: "element"
 	});
+	
 });
