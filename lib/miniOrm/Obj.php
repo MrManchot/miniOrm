ff3bf872d0c5e335c47a468f4792bea6090fcdb4
Jorr.it <jorrit@jorr.it>
2020-11-07 14:37:13 +0100

    DateTime object accepted for datetime field

--- lib/miniOrm/Obj.php
+++ lib/miniOrm/Obj.php
@@ -198,6 +198,9 @@ class Obj
             if (is_array($value)) {
                 $value = serialize($value);
             }
+            else if ($this->vDescribe[$key]['type'] == 'datetime' && $value instanceof \DateTime) {
+                $value = $value->format('Y-m-d H:i:s.u');
+            }
             $this->v[$key] = $value;
         } else {
             $this->vmax[$key] = $value;
